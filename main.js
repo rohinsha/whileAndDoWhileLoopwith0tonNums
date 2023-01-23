d8bcfd6e97daa545505946c54d6158e78fbe7143
rohinsha <r2hinsha@gmail.com>
2023-01-23 05:31:11 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
+
+console.log("Welcome to Programiz!");
+
+function toCountWhile() {
+  let n = 0;
+  while (n <= 6) {
+    console.log(`${n} + while loop`);
+    n += 1;
+  }
+}
+
+toCountWhile();
+
+function toCountDoLoop() {
+  let n = 0;
+  do {
+    console.log(`${n} + do while loop`);
+    n += 1;
+  } while (n <= 6);
+}
+
+toCountDoLoop();
